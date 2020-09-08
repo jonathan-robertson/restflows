cf15e7a511d429bb64d831faca5070922239daab
Jonathan Robertson <jrob356@icloud.com>
2020-09-08 09:48:21 -0500

    refactor deserialize, add go command

--- src/main/java/com/jonathanrobertson/restflows/commands/RestCommands.java
+++ src/main/java/com/jonathanrobertson/restflows/commands/RestCommands.java
@@ -4,10 +4,10 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.StringJoiner;
 
+import lombok.AllArgsConstructor;
+
 import org.springframework.boot.json.JsonParser;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.web.client.RestTemplate;
@@ -15,23 +15,61 @@ import org.springframework.web.client.RestTemplate;
 import com.jonathanrobertson.restflows.services.MemoryService;
 
 @ShellComponent
+@AllArgsConstructor
 public class RestCommands {
-	private static final StringJoiner	errJoiner		= new StringJoiner("; ", "[!] request failed", "");
-	private static final StringJoiner	newlineJoiner	= new StringJoiner("\n");
+	private static final StringJoiner errJoiner = new StringJoiner("; ", "[!] request failed", "");
 
 	private final JsonParser	jsonParser;
 	private final RestTemplate	rest;
 	private final MemoryService	mem;
 
-	public RestCommands(JsonParser jsonParser, RestTemplate rest, MemoryService mem) {
-		this.jsonParser = jsonParser;
-		this.rest = rest;
-		this.mem = mem;
+	@ShellMethod("Submit a REST request to the provided place")
+	public String go(String method, String place/*
+												 * , @ShellOption(defaultValue = "__NONE__") String
+												 * body, @ShellOption(defaultValue = "__NONE__") MultiValueMap<String, String>
+												 * headers
+												 */) {
+		return deserialize(request(method, place, /* new HttpEntity<>(body, headers) */ null));
 	}
 
 	@ShellMethod("Retrieve GET from the provided place")
 	public String get(String place) {
-		return deserialize(request(HttpMethod.GET, place));
+		return deserialize(request("get", place, null));
+	}
+
+	@ShellMethod("Retrieve HEAD from the provided place")
+	public String head(String place) {
+		return request("head", place, null).getHeaders().toString();
+	}
+
+	@ShellMethod("Retrieve POST from the provided place")
+	public String post(String place, String body) { // TODO
+		return request("post", place, null).getBody();
+	}
+
+	@ShellMethod("Retrieve PUT from the provided place")
+	public String put(String place, String body) { // TODO
+		return request("put", place, null).getBody();
+	}
+
+	@ShellMethod("Retrieve PATCH from the provided place")
+	public String patch(String place, String body) { // TODO
+		return request("patch", place, null).getBody();
+	}
+
+	@ShellMethod("Retrieve DELETE from the provided place")
+	public String delete(String place) {
+		return request("delete", place, null).getBody();
+	}
+
+	@ShellMethod("Retrieve OPTIONS from the provided place")
+	public String options(String place) {
+		return request("options", place, null).getBody();
+	}
+
+	@ShellMethod("Retrieve TRACE from the provided place")
+	public String trace(String place) {
+		return request("trace", place, null).getBody();
 	}
 
 	protected Object follow(String data, String key) {
@@ -49,53 +87,25 @@ public class RestCommands {
 			return "[no body provided in response]";
 		}
 
-		return Optional.ofNullable(r.getHeaders().getContentType())
+		return Optional.of(r)
+				.map(ResponseEntity::getHeaders)
+				.map(HttpHeaders::getContentType)
 				.filter(type -> type.equalsTypeAndSubtype(MediaType.APPLICATION_JSON))
-				.map(type -> {
-					jsonParser.parseMap(r.getBody()).forEach((k, v) -> newlineJoiner.add(k + ": " + v));
-					return newlineJoiner.toString();
-				})
-				.orElse(r.getBody());
+				.isPresent()
+						? processBody(r.getBody())
+						: r.getBody();
 	}
 
-	@ShellMethod("Retrieve HEAD from the provided place")
-	public String head(String place) {
-		return request(HttpMethod.HEAD, place).getHeaders().toString();
+	protected String processBody(String body) {
+		final StringJoiner newlineJoiner = new StringJoiner("\n");
+		jsonParser.parseMap(body).forEach((k, v) -> newlineJoiner.add(k + ": " + v));
+		return newlineJoiner.toString();
 	}
 
-	@ShellMethod("Retrieve POST from the provided place")
-	public String post(String place, String body) { // TODO
-		return request(HttpMethod.POST, place).getBody();
-	}
-
-	@ShellMethod("Retrieve PUT from the provided place")
-	public String put(String place, String body) { // TODO
-		return request(HttpMethod.PUT, place).getBody();
-	}
-
-	@ShellMethod("Retrieve PATCH from the provided place")
-	public String patch(String place, String body) { // TODO
-		return request(HttpMethod.PATCH, place).getBody();
-	}
-
-	@ShellMethod("Retrieve DELETE from the provided place")
-	public String delete(String place) {
-		return request(HttpMethod.DELETE, place).getBody();
-	}
-
-	@ShellMethod("Retrieve OPTIONS from the provided place")
-	public String options(String place) {
-
-		return request(HttpMethod.OPTIONS, place).getBody();
-	}
-
-	@ShellMethod("Retrieve TRACE from the provided place")
-	public String trace(String place) {
-		return request(HttpMethod.TRACE, place).getBody();
-	}
-
-	private ResponseEntity<String> request(HttpMethod method, String place) {
-		// TODO: check out https://docs.postman-echo.com/?version=latest
-		return rest.exchange(mem.getPlaces().get(place).getUriTemplate(), method, null, String.class, mem.getVars());
+	// TODO: check out https://docs.postman-echo.com/?version=latest
+	private ResponseEntity<String> request(String method, String place, HttpEntity<String> requestEntity) {
+		HttpMethod httpMethod = HttpMethod.valueOf(method.toUpperCase());
+		String uriTemplate = mem.getPlaces().get(place).getUriTemplate();
+		return rest.exchange(uriTemplate, httpMethod, requestEntity, String.class, mem.getVars());
 	}
 }
